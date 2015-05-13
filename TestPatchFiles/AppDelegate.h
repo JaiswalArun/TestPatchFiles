8785043f0f384da6feea7608e84fc05272da0c6a
JaiswalArun <persistent@persistents-MacBook-Pro.local>
2015-05-13 15:16:09 +0530

    Initial Commit

---
+++ TestPatchFiles/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TestPatchFiles
+//
+//  Created by Arun Jaiswal on 5/13/15.
+//  Copyright (c) 2015 Arun Jaiswal. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ TestPatchFiles/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  TestPatchFiles
+//
+//  Created by Arun Jaiswal on 5/13/15.
+//  Copyright (c) 2015 Arun Jaiswal. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
